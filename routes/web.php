fd584615f637cf338016f143b59db372b9b2830d
永山　莉菜 <2019162n@jissen.ac.jp>
2026-09-25 07:44:37 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::post('/reset-password', [AuthController::class, 'resetPassword'])->name('
 Route::get('admin/login', [AdminController::class, 'showLoginForm'])->name('admin.login');
 Route::post('admin/login', [AdminController::class, 'login'])->name('admin.login.submit');
 
-// 管理者専用ルート（上部：ホームやユーザー管理など）
+// 管理者
 Route::middleware(['auth'])->prefix('admin')->name('admin.')->group(function () {
     Route::get('/home', [AdminController::class, 'home'])->name('home');
     Route::post('/logout', [AdminController::class, 'logout'])->name('logout');
